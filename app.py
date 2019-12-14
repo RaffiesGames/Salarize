42e73a0140994ac6d318985a55e8c5239b7990cd
Raahul S <rafs1801@gmail.com>
2019-12-14 12:44:14 +0530

    Main function
    To accept dynamic port from heroku

--- app.py
+++ app.py
@@ -175,6 +175,8 @@ def analyze():
       #result_prediction='hello'
         return render_template("index.html",result_prediction_l=result_prediction_l,result_prediction_u=result_prediction_u)
      
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
